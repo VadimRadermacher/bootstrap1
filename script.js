cf44d7b3e70ffda0ed159675d2cf287214758af0
Vadim Radermacher <vadim.radermacher@gmail.com>
2019-02-15 14:34:23 +0100

    first

--- script.js
+++ script.js
@@ -8,5 +8,20 @@ import 'bootstrap';
 /*
   Put the JavaScript code you want below.
 */
+$('#myCarousel').carousel();
+ var winWidth = $(window).innerWidth();
+ $(window).resize(function () {
 
+     if ($(window).innerWidth() < winWidth) {
+         $('.carousel-inner>.item>img').css({
+             'min-width': winWidth, 'width': winWidth
+         });
+     }
+     else {
+         winWidth = $(window).innerWidth();
+         $('.carousel-inner>.item>img').css({
+             'min-width': '', 'width': ''
+         });
+     }
+ });
 console.log("Hey look in your browser console. It works!");
